741226c433c9e700f043410fbdc0143032cfc12f
Ahdrianne <ahdrianne190@gmail.com>
2023-03-05 13:55:11 +0800

    New Product details page (html), (css), and test image for product

---
+++ product_details.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/product_details.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/fontawesome-free-6.2.1-web/css/all.min.css">
+    <link rel="icon" href="images/icon.png">
+    <title>On your feet | Product Details</title>
+</head>
+<body id="product_details" class="pdetails">
+
+    <?php include "header.php"; ?>
+    
+    <section class="pdetails bg-img">
+        <div class="container ">
+
+            <div class="row ">
+
+                <div class="col-lg-6 col-md-6 col-sm-12 imgbox">
+                    <img src="product_test_image.png" class="img-fluid" alt="Product Image">
+                </div>
+
+                <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="productName details-right">Product Name HERE</h1>
+
+                    </div>
+
+                </div>
+                    <div class="row rbox">
+                        <div class="col">
+                            <h3 class="details-right">
+                            Proof that quality still exists, our Made in the USA 990v5 is the ultimate blend of performance and style. Made without compromise, the 990v5 is a staple of both morning runs and fashion runways.
+                            New Balance MADE U.S. footwear contains a domestic value of 70% or more. MADE makes up a limited portion of New Balance’s U.S. sales.
+
+                            Features
+                            • Suede and mesh upper
+                            • Dual density collar foam offers support and comfort for ankles
+                            • ENCAP midsole cushioning combines lightweight foam with a durable polyurethane rim to deliver all-day support
+                            • All-day comfort with Ortholite® insert and firm -yet- supportive midsole
+                            • Blown rubber outsole provides superior rebound
+                            • 12 mm drop; due to variances created during the development and manufacturing processes, all references to 12 mm drop are approximate
+                            • New Balance MADE U.S. footwear contains a domestic value of 70% or more. MADE makes up a limited portion of New Balance’s U.S. sales.
+
+                            Available Colors: Gray/Black/Navy
+                            Available Sizes: EU/US/UK
+                            </h3>
+                            <h2>$91199.99 (PRICE HERE)</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                        <button class="btn btn-primary btn-lg btn-block">Add to Cart</button>
+                        </div>           
+                    </div>
+                        
+                </div>
+
+            </div>
+            
+        </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </section>
+
+    <?php include "footer.php"; ?>
+
+    <script defer src="js/script.js"></script>
+
+</body>
+</html>
