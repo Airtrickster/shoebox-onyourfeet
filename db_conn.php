93417cdd355dc6508bd2f478aa243941bdcb5aa2
jamez2128 <jamez2128@localhost>
2023-03-11 00:52:21 +0800

    changed formatting in database link

--- db_conn.php
+++ db_conn.php
@@ -1,4 +1,3 @@
 <?php
-    $link=mysqli_connect("localhost", "root", "") or die(mysqli_error($link));
-    mysqli_select_db($link, "onyourfeet");
+    $link=mysqli_connect("localhost", "root", "", "onyourfeet") or die(mysqli_error($link));
 ?>
