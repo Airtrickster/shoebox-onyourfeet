f999d2acf17eff4128d1d41d2ea8729f583cd12e
jamez2128 <jamez2128@localhost>
2023-03-29 22:16:44 +0800

    forgot to change variable in statement

--- check_item.php
+++ check_item.php
@@ -2,7 +2,7 @@
     function itemExists($table, $column, $productId) {
         session_start();
         include "db_conn.php";
-        $checkItemstmt = mysqli_prepare($link, "SELECT DISTINCT $column FROM $table WHERE user_id = ? AND product_id = ?;");
+        $checkItemstmt = mysqli_prepare($link, "SELECT DISTINCT $column FROM $table WHERE user_id = ? AND $column = ?;");
         mysqli_stmt_bind_param($checkItemstmt, "ii" , $_SESSION["user_id"], $productId);
         mysqli_stmt_execute($checkItemstmt);
         $checkItemResults = mysqli_stmt_get_result($checkItemstmt);
