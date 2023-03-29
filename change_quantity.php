335f5f01f4e1c8dd60e623c3a181083fa42daf49
jamez2128 <jamez2128@localhost>
2023-03-29 22:15:13 +0800

    Changed change quantity from product id to item id

--- change_quantity.php
+++ change_quantity.php
@@ -2,11 +2,11 @@
     session_start();
     include "db_conn.php";
     include "check_item.php";
-    if (! isset($_SESSION["user_id"]) || ! itemExists("cart", "product_id", $_GET["product_id"])) {
+    if (! isset($_SESSION["user_id"]) || ! itemExists("cart", "item_id", $_GET["item_id"])) {
         echo '<script> window.location.href="login-signup.php" </script>';
     }
-    $changeQuantitystmt = mysqli_prepare($link, "UPDATE cart SET quantity = ? WHERE user_id = ? AND product_id = ?;");
-    mysqli_stmt_bind_param($changeQuantitystmt, "iii", $_GET["quantity"], $_SESSION["user_id"], $_GET["product_id"]);
+    $changeQuantitystmt = mysqli_prepare($link, "UPDATE cart SET quantity = ? WHERE user_id = ? AND item_id = ?;");
+    mysqli_stmt_bind_param($changeQuantitystmt, "iii", $_GET["quantity"], $_SESSION["user_id"], $_GET["item_id"]);
     mysqli_stmt_execute($changeQuantitystmt);
 
 
