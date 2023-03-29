ca3b34035a2dba9e845c802df71926b63f30475d
jamez2128 <jamez2128@localhost>
2023-03-29 22:17:24 +0800

    Added a validation when adding to cart

--- add_to_cart.php
+++ add_to_cart.php
@@ -3,10 +3,15 @@
     include "db_conn.php";
     include "check_item.php";
     if (! isset($_SESSION["user_id"])) { echo '<script> window.location.href="login-signup.php" </script>'; }
-    
-    if (itemExists("cart", "product_id", $_GET["product_id"])) { echo '<script> history.back(); </script>'; exit; }
-    $addToCartstmt = mysqli_prepare($link, "INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, 1);");
-    mysqli_stmt_bind_param($addToCartstmt, "ii", $_SESSION["user_id"], $_GET["product_id"]);
+    if (isset($_GET["quantity"]) && is_numeric($_GET["quantity"]) && $_GET["quantity"] >= 1) {
+        $quantity = $_GET["quantity"];
+    } else {
+        $quantity = 1;
+    }
+
+    if (itemExists("cart", "product_id" , $_GET["product_id"])) { echo '<script> history.back(); </script>'; exit; }
+    $addToCartstmt = mysqli_prepare($link, "INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?);");
+    mysqli_stmt_bind_param($addToCartstmt, "iii", $_SESSION["user_id"], $_GET["product_id"], $quantity);
     mysqli_stmt_execute($addToCartstmt);
 ?>
 
