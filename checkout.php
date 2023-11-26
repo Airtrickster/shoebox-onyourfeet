e4b177b60accf7e3cf11ae1642497bba8c9ac50f
jamez2128 <jamez2128@localhost>
2023-11-26 18:51:53 +0800

    Added conditions to checkout script

--- checkout.php
+++ checkout.php
@@ -3,7 +3,10 @@
     session_start();
 }
     include "db_conn.php";
-    echo '<script> console.log("' . $_POST["payment-method"] . '"); </script>';
+    if (! isset($_POST["address_id"]) || ! isset($_POST["payment-method"])) {
+        echo '<script> window.location.href=\'index.php\' </script>';
+    }
+
     $checkoutstmt = mysqli_prepare($link, "INSERT INTO transactions(user_id, address, product_id, quantity, amount, payment_method) SELECT user_id, (SELECT CONCAT(address, ', ', city, ', ', state, ', ', country, ', ', zip_code) FROM addresses WHERE address_id = ?), product_id, quantity, (SELECT price  FROM products WHERE product_id = cart.product_id), ? FROM cart WHERE user_id = ?;");
     mysqli_stmt_bind_param($checkoutstmt, "isi", $_POST["address_id"], $_POST["payment-method"], $_SESSION["user_id"]);
     mysqli_execute($checkoutstmt);
