374686197062d348219cb87b0a84f3c07ecb4a5d
jamez2128 <jamez2128@localhost>
2023-12-04 21:03:29 +0800

    Added current page in admin nav

--- admin_nav.php
+++ admin_nav.php
@@ -1,9 +1,27 @@
 <aside class="flex side-bar">
 <nav class="sidebtns">
 
+<?php
+  switch ($_SERVER["SCRIPT_NAME"]) {
+  case "/product_maintenance.php":
+    $productMaintenanceActive = "<b>";
+    $productMaintenanceActiveEnd = "</b>";
+    break;
+  case "/user_maintenance.php":
+    $userMaintenanceActive = "<b>";
+    $userMaintenanceActiveEnd = "</b>";
+    break;
+  case "/admin_transactions.php":
+    $transactionsActive = "<b>";
+    $transactionsActiveEnd = "</b>";
+    break;
+  }
+?>
+
 <ul>
-  <li><a href="product_maintenance.php">Product Maintenance</a></li>
-  <li><a href="admin_transactions.php">Transactions</a></li>
+  <li><a href="product_maintenance.php"><?php echo $productMaintenanceActive; ?>Product Maintenance<?php echo $productMaintenanceActiveEnd; ?></a></li>
+  <li><a href="admin_transactions.php"><?php echo $transactionsActive; ?>Transactions<?php echo $transactionsActiveEnd; ?></a></li>
+  <li><a href="user_maintenance.php"><?php echo $userMaintenanceActive; ?>User Maintenance<?php echo $userMaintenanceActiveEnd; ?></a></li>
   <li><a id="drop">Settings</a>
     <ul class="down">
       <li><a href="account.php">My Account</a></li>
